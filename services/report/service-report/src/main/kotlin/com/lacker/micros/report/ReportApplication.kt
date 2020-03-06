9937d1812cb52c74c029bc1f7064d97f39336a66
七月 <qiy@ywsoftware.com>
2020-03-06 19:21:22 +0800

    加入 kotlin 依赖

--- services/report/service-report/src/main/kotlin/com/lacker/micros/report/ReportApplication.kt
+++ services/report/service-report/src/main/kotlin/com/lacker/micros/report/ReportApplication.kt
@@ -1,2 +1,11 @@
 package com.lacker.micros.report
 
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ReportApplication
+
+fun main(args: Array<String>) {
+    runApplication<ReportApplication>(*args)
+}
