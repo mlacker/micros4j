23b91749bfe62abdf78b5d9fc9c2d14883b7cfe4
七月 <mlacker@163.com>
2020-03-29 18:00:10 +0800

    feat(form): create form project

---
+++ services/form/service-form/src/main/kotlin/com/lacker/micros/form/FormApplication.kt
@@ -0,0 +1,11 @@
+package com.lacker.micros.form
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringApplication
+class FormApplication
+
+fun main(args: Array<String>) {
+    runApplication<FormApplication>(*args)
+}
