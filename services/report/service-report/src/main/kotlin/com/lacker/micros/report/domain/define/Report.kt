15c722588effbd69920edffec46f5161a8da9386
七月 <mlacker@163.com>
2020-03-29 17:42:51 +0800

    feat(report): report define

--- services/report/service-report/src/main/kotlin/com/lacker/micros/report/domain/define/Report.kt
+++ services/report/service-report/src/main/kotlin/com/lacker/micros/report/domain/define/Report.kt
@@ -1,4 +1,23 @@
 package com.lacker.micros.report.domain.define
 
-class Report {
+import com.lacker.micros.report.domain.datasource.*
+
+class Report(
+        override val id: Long,
+        var name: String,
+        val table: SourceTable,
+        val createdBy: String,
+        val createdDate: String
+) : SourceTable {
+    val joins: List<Join> = listOf()
+    val columns: List<ReportColumn> = listOf()
+    val filters: List<Filter> = listOf()
+    val groupBies: List<DataColumnOfAlias> = listOf()
+    val orderBies: List<OrderBy> = listOf()
+
+    var lastModifiedBy: String? = null
+    var lastModifiedDate: String? = null
+
+    override val primaryKeyId get() = "id"
+    val groupByEnabled get() = this.groupBies.isNotEmpty()
 }
