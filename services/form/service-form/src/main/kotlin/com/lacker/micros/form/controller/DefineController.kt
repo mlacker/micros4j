183af670fc4d197a7c2b7ee21559389d5acf2679
七月 <mlacker@163.com>
2020-04-21 05:21:27 +0800

    feat(form): add ctrl and service

---
+++ services/form/service-form/src/main/kotlin/com/lacker/micros/form/controller/DefineController.kt
@@ -0,0 +1,27 @@
+package com.lacker.micros.form.controller
+
+import com.lacker.micros.domain.entity.PaginatedFilter
+import com.lacker.micros.form.api.model.define.DefineModel
+import com.lacker.micros.form.service.DefineService
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/defines")
+class DefineController(
+        private val service: DefineService
+) {
+    @GetMapping(params = ["page", "size"])
+    fun findByFilter(filter: PaginatedFilter) = service.findByFilter(filter)
+
+    @GetMapping
+    fun findAll() = service.findAll()
+
+    @GetMapping("{id}")
+    fun find(@PathVariable id: Long) = service.find(id)
+
+    @PostMapping
+    fun save(@RequestBody model: DefineModel) = service.save(model)
+
+    @DeleteMapping
+    fun delete(@PathVariable id: Long) = service.delete(id)
+}
